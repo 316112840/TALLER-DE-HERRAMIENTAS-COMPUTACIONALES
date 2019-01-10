f9d795847a784f7a199237f65bafa81a2cdbd048
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-10 16:30:45 -0600

    Este es el primer ejemplo de programaen Python

---
+++ Clases/Programas/Ejemplo1Pelota.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Martínez García Mariana Yasmin
+print (34*3)-(1/2)*(9.81)*(3**2)#1/2 calcula la división entera
+print (34*3)-(1/2.0)*(9.81)*(3**2)#1.0/2 o 1/2.0 división flotante
+print (34*1)-(1/2.0)*(9.81)*(1*2)
+print (34*1.5)-(1/2.0)*(9.81)*(1.5**2)
+print (34*5)-(1/2.0)*(9.81)*(5**2)
+v0 = 34
+g = 9.81
+t = 5
+y = v0*t - 1.0/2*g*t**2
+print y
