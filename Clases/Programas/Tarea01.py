d7bb57f984da8f5b69858d646e5fa0e6ae68ed01
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-10 17:14:53 -0600

    Esta es la tarea 1

---
+++ Clases/Programas/Tarea01.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Martínez García Mariana Yamsin
+# Ejercicio sobre tiro vertical
+V1 = 0 #Velocidad final
+H = 3.2 #Altura máxima alcanzada
+m = 0 #Altura inicial
+a = 9.81 #La aceleración es la fuerza de gravedad
+#V1=V0**2 - 2*a*(H-h)
+import math
+V0 = math.sqrt(V1 + 2*a*(H - m))
+
+print V0
