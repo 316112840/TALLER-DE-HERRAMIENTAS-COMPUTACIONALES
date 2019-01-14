11b861e383ff3882717225d81e29ed5189366184
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-14 16:57:27 -0600

    Estos son dos programas, uno sobre valor absoluto y el otro es una Diana

---
+++ Clases/Programas/funcion01.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+def vAbsoluto(x):
+    if x>=0:
+        return(x)
+    else:          #si el bloque (lo que está depués de los dos puntos) "if" es falso se hará lo de este bloque.
+        return(-x) 
+    
+
+
+
+
+
+
+
+
+
+#que algo haga eco es que muestre el resultado
---
+++ Diana.py
@@ -0,0 +1,14 @@
+def Diana(x,y):
+    if x>=5 and x<=5 and y<10:
+        return(7)
+    elif x>=5 and x<=5 and y>30:
+        return(7)
+    elif x<5 and y<=30 and y>=10:
+        return(5)
+    elif x>25 and y>=10 and y<=30:
+        return(5)
+    elif x>=5 and x<=25 and y<=30 and y>=10:
+        return(10)
+    else:
+        return(3)
+
