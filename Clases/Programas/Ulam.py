088920529bf53d999b5428ba99ad1a59854f5f93
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-15 16:17:40 -0600

    Estas son dos versiones de Ulam

--- Clases/Programas/Ulam.py
+++ Clases/Programas/Ulam.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
-def Ulam(x):
-    i = 0
-    while x>1:
-        if (x/2)*2 -x==0:
-            x = x/2
-        else:
-            x= 3*x +1
+#Esta es la versión del profesor de Ulam
+def ulam(x):
+    #if (x/2)*-x == 0:
+    if x%2 == 0:
+        return x/2
+    else:
+        return 3*x + 1
+
+def suc(x):
+    i=0
+    #while x>1:
+    while x!=1:
+        x=ulam(x)
         i = i+1
-    print"El ciclo se repitió %d veces"%(i)
-    return(x)
+    return i
+
+
+print suc(7)
+print suc(26)
+print suc(52)
+print suc(1024)
+print suc(72)
+print suc(1524927)
+print suc(2)
         
         
---
+++ Clases/Programas/Ulam0.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#Esta es mi versión de Ulam
+def Ulam0(x):
+    i = 0
+    while x>1:
+        if (x/2)*2 -x==0:
+            x = x/2
+        else:
+            x= 3*x +1
+        i = i+1
+    print"El ciclo se repitió %d veces"%(i)
+    
+        
+        
+Ulam0(7)
+Ulam0(26)
+Ulam0(52)
+Ulam0(1024)
+Ulam0(72)
+Ulam0(1524927)
+Ulam0(2)
