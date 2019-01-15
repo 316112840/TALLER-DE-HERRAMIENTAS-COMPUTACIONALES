bdbc3ff303c540d22d2b6971697a6bcd50266e77
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-15 10:18:40 -0600

    Este es el problema de la tarea número 03

--- Clases/Programas/Tarea03.py
+++ Clases/Programas/Tarea03.py
@@ -17,7 +17,7 @@ def Diana(x,y):
         return(3)
     elif x>25 and y<10:
         return(3)
-    elif x>=5 and x<=25 and y<=30 and y>=10 and -525>= x**2 -10*x + y**2 -40*y:
+    elif (x-15)**2 + (y-20)**2 < 100:
         return(10)
     else:
         return(100)
