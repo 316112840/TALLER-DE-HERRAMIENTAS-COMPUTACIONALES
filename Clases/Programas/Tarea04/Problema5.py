5eb0f747de62ccdf57a0a2aca6da3d360b6d6fc1
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-18 10:48:23 -0600

    Estos son los archivos correspondientes al Problema 1 de la tarea 4

--- Clases/Programas/Tarea04/Problema5.py
+++ Clases/Programas/Tarea04/Problema5.py
@@ -6,4 +6,13 @@ Taller de Herramientas Computacionales
 Problema 5'''
 
 
-def SumaN(a):
+def SumaN(n):
+    a = 0
+    b = 1
+    c = a + b
+    while n >= 0:
+        n += c
+    print(n)
+    
+#n += a   
+#n= a +n
