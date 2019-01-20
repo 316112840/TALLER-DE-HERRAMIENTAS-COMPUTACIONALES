fcb56cb164096f463fd06872d108b87a5252f1ab
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-20 02:38:04 -0600

    Estos son los archivos correspondientes al Problema 4 de la tarea 4

--- Clases/Programas/Tarea04/Problema4.py
+++ Clases/Programas/Tarea04/Problema4.py
@@ -3,4 +3,30 @@
 ''' Marínez García Mariana Yasmin
 316112840
 Taller de Herramientas Computacionales
-Problema 4'''
+Problema 4: Sucesión de Fibonacci'''
+
+#0 1 1 2 3 5 8 13 21 34 55 89 144
+
+def Fibonacci(n):
+    i = 0
+    A0 = 0
+    A1 = 1
+    A2 = 1
+    A3 = 1
+    if n%2 == 0:
+        n = n//2
+        while n  > i :
+            i = i + 1
+            A0 = A0 + A1
+            A1 = A1 + A0
+        print "El número de la sucesión que corresponde al dígito %d es: %d" %(n*2,A1)
+    else:
+        n = n//2
+        while n > i :
+            i = i + 1
+            A2 = A2 + A3
+            A3 = A3 + A2
+        print "El número de la sucesión que corresponde al dígito %d es: %d" %(n*2 + 1, A3)
+
+
+    
---
+++ Clases/Programas/Tarea04/Problema4S.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+''' Marínez García Mariana Yasmin
+316112840
+Taller de Herramientas Computacionales
+Problema 4 soluciones'''
+
+import Problema4
+
+x = input("Ingrese el dígito: ")
+print Problema4.Fibonacci(x)
+
+
+
+print Problema4.Fibonacci(0)
+print Problema4.Fibonacci(1)
+print Problema4.Fibonacci(2)
+print Problema4.Fibonacci(3)
+print Problema4.Fibonacci(4)
+print Problema4.Fibonacci(5)
+print Problema4.Fibonacci(6)
+print Problema4.Fibonacci(7)
+print Problema4.Fibonacci(8)
+print Problema4.Fibonacci(9)
+print Problema4.Fibonacci(10)
+print Problema4.Fibonacci(11)
+print Problema4.Fibonacci(12)
+print Problema4.Fibonacci(13)
