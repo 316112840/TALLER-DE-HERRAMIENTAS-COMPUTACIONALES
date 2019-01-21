c6fe9936fb5dfa8119046768b168e65e7dd26803
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-20 18:31:02 -0600

    Estos son archivos de la tarea 4

--- Clases/Programas/Tarea04/Problema2.py
+++ Clases/Programas/Tarea04/Problema2.py
@@ -4,3 +4,14 @@
 316112840
 Taller de Herramientas Computacionales
 Problema 2'''
+
+import math
+def tiempo(v0,h):
+    a = 9.81
+    v0 = v0
+    h = h
+    t1 = (v0 + math.sqrt(v0**2 - 2*a*h))/a
+    t2 = (v0 - math.sqrt(v0**2 - 2*a*h))/a
+    print "Los tiempos donde se alcanza la altura %f m son %f y %f"%(h, t1, t2)
+
+
