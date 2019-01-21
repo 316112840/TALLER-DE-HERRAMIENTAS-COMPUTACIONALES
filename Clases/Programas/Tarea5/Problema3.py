6243f423aacd6199cf4954cad4f619368887e889
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-21 10:24:39 -0600

    Esto es parte de la tarea5

---
+++ Clases/Programas/Tarea5/Problema3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+''' Marínez García Mariana Yasmin
+316112840
+Taller de Herramientas Computacionales
+Problema 3 con listas '''
+
+#NO ESTÁ TERMINADO
+
+def Cel(c):
+    C = []
+    f = c*9/5 +32
+    C.append(f)
+    for f in C:
+        print f      
+    print '%5d %5.2f' % (c, f)
+
+
---
+++ Clases/Programas/Tarea5/Problema3S.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+''' Marínez García Mariana Yasmin
+316112840
+Taller de Herramientas Computacionales
+Problema 3 con listas. Soluciones '''
+#NO ESTÁ TERMINADO
+import Problema3
+
+c = input("Ingrese el grado Celsius: ")
+S = "N"
+if c != S :
+    while a != S:
+        a = input("Ingrese el grado Celsius: ")
+        if a == S:
+            break
+        print "     C     F"
+        print Problema3.cel(a)
+    print Problema3.Cel(c)
