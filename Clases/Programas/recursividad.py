c73d8a62cc1b1f54dd52585c99d1ef7b2633ebb0
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-23 17:01:36 -0600

    Esto es lo que se hizo la clase 13

--- Clases/Programas/recursividad.py
+++ Clases/Programas/recursividad.py
@@ -4,6 +4,7 @@
 316112840
 Taller de Herramientas Computacionales
 Esto es lo que se vió el miércoles 23 de enero. Recursividad'''
+#http://www.pythontutor.com/visualize.html  en esta página podemos ver el procedimiento de cómo se está ejecutando un programa.
 
 def fib(n):
     """ Calculo el nesimo termino de la suc
@@ -23,12 +24,19 @@ def Suma(n):
     else :
         return 1
 
-def printR(L):
+def printR(L): #Cuando la longitud de L es mayor a 1, mostrará el primero y procesará el resto. Y así sucesivamente hasta que solo haya uno y lo va a mostrar.
     if len(L)>1:
-        print L[0],
+        print L[0], #La coma (,) omite el salto de línea
         printR(L[1:])
     else:
         print L[0]
     
+def printr(L):
+    if L: #En este caso se usa lo de "Si una lista es vacía se toma como falso"
+        print L[0],
+        printr(L[1:])
+
 L = [1,2,4,5,9]
-printR(L) 
+printR(L)
+printr(L)
+#printR y printr son dos formas de hacer lo mismo.
