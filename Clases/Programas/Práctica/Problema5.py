18c50933376084110e4661830682c37c8f281507
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-25 16:51:57 -0600

    Examen práctico

--- Clases/Programas/Práctica/Problema5.py
+++ Clases/Programas/Práctica/Problema5.py
@@ -5,6 +5,22 @@
 Taller de Herramientas Computacionales
 Examen práctico. Problema5'''
 
+from Problema4 import Range
 
-def f(x,y):
+def f(x1,x2,a,y1,y2,b):
+    F = []
+    F1 = []
+    F2= []
+    h = 0
+    for i in Range(x1,x2,a):
+        F.append(i)
+    for j in Range(y1,y2,b):
+        F1.append(j)
+    g = len(F)*len(F1)
+    while h < g:
+        h += 1
+        f = (F[h]**2)/25.0 - (F[h]**2)/49.0
+        F2.append(f)
+    print F2
+    print F, F1, g, F2
     
