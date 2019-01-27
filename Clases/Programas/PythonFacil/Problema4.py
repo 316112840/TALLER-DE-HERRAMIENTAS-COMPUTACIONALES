cf39493db22a2f6f34097e93fea662ae9606f159
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-26 23:37:58 -0600

    Python facil

---
+++ Clases/Programas/PythonFacil/Problema4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+''' Marínez García Mariana Yasmin
+316112840
+Taller de Herramientas Computacionales
+Problema 4 del libro Python facil, página 58'''
+def linfinita(x):
+    a = 0
+    L = []
+    while a > 0:
+        a += 1
+        y = a*x
+        L.append(y)
+        print L
+    
+
+
+print linfinita(5)
+
+#NO SE PUEDE ASÍ
---
+++ Clases/Programas/PythonFacil/Problema7.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+''' Marínez García Mariana Yasmin
+316112840
+Taller de Herramientas Computacionales
+Problema 7 del libro Python facil, página 58'''
+
+def f(l):
+    a = 0
+    b = 0
+    for i in l:
+        if i > 0:
+            a += i
+        else:
+            a -= i
+    return a + b
+
+"""Al ingresar una lista "l", si "l" tiene más de 0 números dentro de ella los sumará.
+si esto no se cumple, es decir, si tiene 0 elementos le restará "b" y como b es igual
+a 0 te mostrará el resultado de restar 0 menos 0, es decir, 0"""
