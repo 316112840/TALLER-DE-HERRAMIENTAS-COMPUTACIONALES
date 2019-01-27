60961b79c1b82233c92a1b14bc456798c86f9681
316112840 <mariana_yasmin@ciencias.unam.mx>
2019-01-27 00:56:01 -0600

    Python facil, laberinto. Pero no está completo

--- Clases/Programas/PythonFacil/Problema5.py
+++ Clases/Programas/PythonFacil/Problema5.py
@@ -5,17 +5,34 @@
 Taller de Herramientas Computacionales
 Problema 5, laberinto'''
 
-L = [[True, True, True], [False, False, False], [True, True, True]]
+L = [[True, True, False, True, True], [False, False, False, True, True], [True, True, True, True, True]]
 
 #L es el laberinto, E es la entrada. Y hay que buscar S que es la salida.
-def laberinto(L, E):
+
+def laberinto(L, e):
     """ Este programa te permitirá
 encontar la salida del laberinto
-que se dé y la entrada"""
-    n = len(L[0])-1 #El laberinto es de n*m
-    m = len(L)-1
-    x = E[0]
-    y = E[1]
-    if x 
+que se dé"""
+    n = len(L[0])  - 1
+    m = len(L) - 1
+    x = e[0]
+    y = e[1]
+    if y == n or x == m:
+        return e[0]+1, e[1]+1 
+    elif y<n and x<m:
+        if L[x][y+1] == False:
+            e = [x, y+1]
+            return laberinto(L, e)
+        elif L[x + 1][y] == False:
+            e = [x+1, y]
+            return laberinto(L, e)
+        #if L[x][y -1] == False:  ESTO ESTÁ MAL
+        #    e = [x, y - 1 ]
+         #   return laberinto(L,e)
+       # elif L[x-1][y] == False : ESTO ESTÁ MAL 
+        #    e = [x-1,y]
+         #   return laberinto(L,e)
+        else:
+            return "Ya no se puede seguir avanzando"
     
-    
+print laberinto(L,[0,1])
